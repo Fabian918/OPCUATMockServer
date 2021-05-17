980dbf66ad5ae5da2fcd9d7d66f4fa9d0ee6a83f
Fabian Hering <Fabian.Hering@plan-b-gmbh.com>
2021-05-17 16:53:19 +0200

    #Inital Commit

---
+++ OPCUAMock/CustomTestNodeManager.cs
@@ -0,0 +1,283 @@
+﻿using Opc.Ua;
+using Opc.Ua.Server;
+using Quickstarts.ReferenceServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace OPCUAMock
+{
+    public class CustomTestNodeManager : CustomNodeManager2
+    {
+        private ReferenceServerConfiguration m_configuration;
+        private Opc.Ua.Test.DataGenerator m_generator;
+        private List<BaseDataVariableState> m_dynamicNodes;
+
+        public AppSettings Settings { get; set; }
+        public CustomTestNodeManager(IServerInternal server, ApplicationConfiguration configuration, AppSettings settings)
+          : base(server, configuration, new[] { "http://opcuamocksersever.org/Namespace" })
+        {
+            Settings = settings;
+            SystemContext.NodeIdFactory = this;
+
+            // get the configuration for the node manager.
+            m_configuration = configuration.ParseExtension<ReferenceServerConfiguration>();
+
+            // use suitable defaults if no configuration exists.
+            if (m_configuration == null)
+            {
+                m_configuration = new ReferenceServerConfiguration();
+            }
+
+            m_dynamicNodes = new List<BaseDataVariableState>();
+        }
+
+
+        public override void CreateAddressSpace(IDictionary<NodeId, IList<IReference>> externalReferences)
+        {
+            lock (Lock)
+            {
+                IList<IReference> references = null;
+
+                if (!externalReferences.TryGetValue(ObjectIds.ObjectsFolder, out references))
+                {
+                    externalReferences[ObjectIds.ObjectsFolder] = references = new List<IReference>();
+                }
+
+                FolderState root = CreateFolder(null, "Root", "Root");
+                root.AddReference(ReferenceTypes.Organizes, true, ObjectIds.ObjectsFolder);
+                references.Add(new NodeStateReference(ReferenceTypes.Organizes, false, root.NodeId));
+                root.EventNotifier = EventNotifiers.SubscribeToEvents;
+                AddRootNotifier(root);
+
+                List<BaseDataVariableState> variables = new List<BaseDataVariableState>();
+
+                try
+                {
+
+                    this.Settings.NodesToCreate.ForEach(node =>
+                    {
+                        if (!string.IsNullOrEmpty(node.Path_s))
+                        {
+                            variables.Add(CreateVariable(root, node.Path_s, node.Name, DataTypes.String, ValueRanks.Scalar));
+                        }
+                        else
+                        {
+                            variables.Add(CreateVariable(root, node.Path_i, node.Name, DataTypes.String, ValueRanks.Scalar));
+                        }
+
+                    });
+
+                    //FolderState junotest = CreateFolder(root, "juno", "juno");
+                    //variables.Add(timNode1 = CreateVariable(junotest, 15056, "RunMode", DataTypes.String, ValueRanks.Scalar));
+                    //variables.Add(timNode2 = CreateVariable(junotest, 15046, "CmmSerialNumber", DataTypes.String, ValueRanks.Scalar));
+                    //variables.Add(timNode3 = CreateVariable(junotest, "CalypsoObservervent/CalypsoObserverInformationNodeId", "CalypsoObserverInformationNodeId", DataTypes.String, ValueRanks.Scalar));
+                 
+
+                }
+                catch (Exception e)
+                {
+                    Utils.Trace(e, "Error creating the address space.");
+                }
+
+                AddPredefinedNode(SystemContext, root);
+            }
+        }
+        /// <summary>
+        /// Creates a new folder.
+        /// </summary>
+        private FolderState CreateFolder(NodeState parent, string path, string name)
+        {
+            FolderState folder = new FolderState(parent);
+
+            folder.SymbolicName = name;
+            folder.ReferenceTypeId = ReferenceTypes.Organizes;
+            folder.TypeDefinitionId = ObjectTypeIds.FolderType;
+            folder.NodeId = new NodeId(path, NamespaceIndex);
+            folder.BrowseName = new QualifiedName(path, NamespaceIndex);
+            folder.DisplayName = new LocalizedText("en", name);
+            folder.WriteMask = AttributeWriteMask.None;
+            folder.UserWriteMask = AttributeWriteMask.None;
+            folder.EventNotifier = EventNotifiers.None;
+
+            if (parent != null)
+            {
+                parent.AddChild(folder);
+            }
+
+            return folder;
+        }
+        /// <summary>
+        /// Creates a new variable.
+        /// </summary>
+        private DataItemState CreateDataItemVariable(NodeState parent, string path, string name, BuiltInType dataType, int valueRank)
+        {
+            DataItemState variable = new DataItemState(parent);
+            variable.ValuePrecision = new PropertyState<double>(variable);
+            variable.Definition = new PropertyState<string>(variable);
+
+            variable.Create(
+                SystemContext,
+                null,
+                variable.BrowseName,
+                null,
+                true);
+
+            variable.SymbolicName = name;
+            variable.ReferenceTypeId = ReferenceTypes.Organizes;
+            variable.NodeId = new NodeId(path, NamespaceIndex);
+            variable.BrowseName = new QualifiedName(path, NamespaceIndex);
+            variable.DisplayName = new LocalizedText("en", name);
+            variable.WriteMask = AttributeWriteMask.None;
+            variable.UserWriteMask = AttributeWriteMask.None;
+            variable.DataType = (uint)dataType;
+            variable.ValueRank = valueRank;
+            variable.AccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.UserAccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.Historizing = false;
+            variable.Value = Opc.Ua.TypeInfo.GetDefaultValue((uint)dataType, valueRank, Server.TypeTree);
+            variable.StatusCode = StatusCodes.Good;
+            variable.Timestamp = DateTime.UtcNow;
+            variable.Value = "";
+
+            if (valueRank == ValueRanks.OneDimension)
+            {
+                variable.ArrayDimensions = new ReadOnlyList<uint>(new List<uint> { 0 });
+            }
+            else if (valueRank == ValueRanks.TwoDimensions)
+            {
+                variable.ArrayDimensions = new ReadOnlyList<uint>(new List<uint> { 0, 0 });
+            }
+
+            variable.ValuePrecision.Value = 2;
+            variable.ValuePrecision.AccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.ValuePrecision.UserAccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.Definition.Value = String.Empty;
+            variable.Definition.AccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.Definition.UserAccessLevel = AccessLevels.CurrentReadOrWrite;
+
+            if (parent != null)
+            {
+                parent.AddChild(variable);
+            }
+
+            return variable;
+        }
+
+        /// <summary>
+        /// Creates a new variable.
+        /// </summary>
+        private BaseDataVariableState CreateVariable(NodeState parent, string path, string name, BuiltInType dataType, int valueRank)
+        {
+            return CreateVariable(parent, path, name, (uint)dataType, valueRank);
+        }
+
+
+        /// <summary>
+        /// Creates a new variable (int Id).
+        /// </summary>
+        private BaseDataVariableState CreateVariable(NodeState parent, uint path, string name, NodeId dataType, int valueRank, string nodeID = null)
+        {
+            BaseDataVariableState variable = new BaseDataVariableState(parent);
+
+            variable.SymbolicName = name;
+            variable.ReferenceTypeId = ReferenceTypes.Organizes;
+            variable.TypeDefinitionId = VariableTypeIds.BaseDataVariableType;
+            variable.NodeId = new NodeId(path, NamespaceIndex);
+            variable.BrowseName = new QualifiedName(path + "", NamespaceIndex);
+            variable.DisplayName = new LocalizedText("en", name);
+            variable.WriteMask = AttributeWriteMask.DisplayName | AttributeWriteMask.Description;
+            variable.UserWriteMask = AttributeWriteMask.DisplayName | AttributeWriteMask.Description;
+            variable.DataType = dataType;
+            variable.ValueRank = valueRank;
+            variable.AccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.UserAccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.Historizing = false;
+            variable.Value = GetNewValue(variable);
+            variable.StatusCode = StatusCodes.Good;
+            variable.Timestamp = DateTime.UtcNow;
+            variable.Value = "";
+
+            if (valueRank == ValueRanks.OneDimension)
+            {
+                variable.ArrayDimensions = new ReadOnlyList<uint>(new List<uint> { 0 });
+            }
+            else if (valueRank == ValueRanks.TwoDimensions)
+            {
+                variable.ArrayDimensions = new ReadOnlyList<uint>(new List<uint> { 0, 0 });
+            }
+
+            if (parent != null)
+            {
+                parent.AddChild(variable);
+            }
+
+            return variable;
+        }
+
+        /// <summary>
+        /// Creates a new variable.
+        /// </summary>
+        private BaseDataVariableState CreateVariable(NodeState parent, string path, string name, NodeId dataType, int valueRank, string nodeID = null)
+        {
+            BaseDataVariableState variable = new BaseDataVariableState(parent);
+
+            variable.SymbolicName = name;
+            variable.ReferenceTypeId = ReferenceTypes.Organizes;
+            variable.TypeDefinitionId = VariableTypeIds.BaseDataVariableType;
+            variable.NodeId = new NodeId(path, NamespaceIndex);
+            variable.BrowseName = new QualifiedName(path, NamespaceIndex);
+            variable.DisplayName = new LocalizedText("en", name);
+            variable.WriteMask = AttributeWriteMask.DisplayName | AttributeWriteMask.Description;
+            variable.UserWriteMask = AttributeWriteMask.DisplayName | AttributeWriteMask.Description;
+            variable.DataType = dataType;
+            variable.ValueRank = valueRank;
+            variable.AccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.UserAccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.Historizing = false;
+            variable.Value = GetNewValue(variable);
+            variable.StatusCode = StatusCodes.Good;
+            variable.Timestamp = DateTime.UtcNow;
+           
+
+            if (valueRank == ValueRanks.OneDimension)
+            {
+                variable.ArrayDimensions = new ReadOnlyList<uint>(new List<uint> { 0 });
+            }
+            else if (valueRank == ValueRanks.TwoDimensions)
+            {
+                variable.ArrayDimensions = new ReadOnlyList<uint>(new List<uint> { 0, 0 });
+            }
+
+            if (parent != null)
+            {
+                parent.AddChild(variable);
+            }
+
+            return variable;
+        }
+
+        private object GetNewValue(BaseVariableState variable)
+        {
+            if (m_generator == null)
+            {
+                m_generator = new Opc.Ua.Test.DataGenerator(null);
+                m_generator.BoundaryValueFrequency = 0;
+            }
+
+            object value = null;
+            int retryCount = 0;
+
+            while (value == null && retryCount < 10)
+            {
+                value = m_generator.GetRandom(variable.DataType, variable.ValueRank, new uint[] { 10 }, Server.TypeTree);
+                retryCount++;
+            }
+
+            return value;
+        }
+
+
+    }
+}
